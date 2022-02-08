4f35bbfef6024393f9a30beb3e9d10fb05b5bdf6
iamstrawberry <85628486+iamstrawberry@users.noreply.github.com>
2022-02-08 15:07:49 +0300

    Add files via upload
    animations and other things

---
+++ rblx miscellaneous scripts/funny.lua
@@ -0,0 +1,7 @@
+local A=Instance.new'Animation'
+A.AnimationId='rbxassetid148840371'
+local P=gameGetService'Players'.LocalPlayer
+local C=P.Character or P.CharacterAddedWait()
+local H=CWaitForChild'Humanoid'LoadAnimation(A)
+HPlay()
+HAdjustSpeed(2.5)
